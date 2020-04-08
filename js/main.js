0294a66c718070e1906969a166f50dd79822f408
Allen Francis <lallenfrancisl@gmail.com>
2020-04-08 17:11:38 +0530

    add navbar, hover effects to button

--- js/main.js
+++ js/main.js
@@ -4,5 +4,27 @@ particlesJS.load('particles', 'js/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
-window.fitText( document.getElementById("company-logo-2"), 0.8);
-window.fitText( document.getElementById("company-logo-1"), 0.8);
+window.fitText( document.getElementById("company-logo-2"), 0.6);
+window.fitText( document.getElementById("company-logo-1"), 0.6);
+
+/**
+ * Navbar
+ */
+var mobileMenuButton = document.querySelector('.navbar-mobile-button');
+var mobileNavbar = document.querySelector('.mobile-navbar');
+var mobileNavbarList = document.querySelectorAll('.mobile-navbar li');
+
+mobileMenuButton.addEventListener('click', function () {
+    if (mobileNavbar.classList.contains('hidden')) {
+        mobileNavbar.classList.remove('hidden');
+    }
+    else {
+        mobileNavbar.classList.add('hidden');
+    }
+});
+
+for (var i = 0; i < mobileNavbarList.length; ++i) {
+    mobileNavbarList[i].addEventListener('click', function () {
+        mobileNavbar.classList.add('hidden');
+    });
+}
