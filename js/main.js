1bf6f65534db92a5dea2e5ad8e08b41dd1793bac
Allen Francis <lallenfrancisl@gmail.com>
2020-04-08 16:41:34 +0530

    add contact section and made company logo responsive

--- js/main.js
+++ js/main.js
@@ -3,3 +3,6 @@ console.log("Connected");
 particlesJS.load('particles', 'js/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+window.fitText( document.getElementById("company-logo-2"), 0.8);
+window.fitText( document.getElementById("company-logo-1"), 0.8);
