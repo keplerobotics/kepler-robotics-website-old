29576e50040f860c76b97878e8d33f55af973b19
Allen Francis <lallenfrancisl@gmail.com>
2020-04-27 18:49:52 +0530

    add training info page and do edits in homepage

--- js/article-page.js
+++ js/article-page.js
@@ -1,10 +1,3 @@
-particlesJS.load('particles', 'js/particlesjs-config.json', function() {
-    console.log('callback - particles.js config loaded');
-});
-
-window.fitText(document.getElementById("company-logo-2"), 0.6);
-window.fitText(document.getElementById("company-logo-1"), 0.6);
-
 /**
  * Navbar
  */
@@ -28,12 +21,4 @@ for (var i = 0; i < mobileNavbarList.length; ++i) {
 
 AOS.init({
     disable: window.innerWidth < 900
-});
-
-var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    typeSpeed: 100,
-    backSpeed: 50,
-    loop: true,
-    loopCount: Infinity
 });
